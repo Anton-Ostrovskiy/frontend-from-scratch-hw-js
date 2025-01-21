ea65140f2f38729a8039f3b19f5973dd535cae13
Anton <ostrovskiy.98@mail.ru>
2025-01-21 19:38:24 +0300

    completed lesson-05 taks 2

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,22 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+// function updateGallery(object,key,newValue) {
+//   if(object.hasOwnProperty(key)){
+//     object[key] = newValue;
+//   }
+//    object[key] = newValue;
+//    return object
+// }
+
+function updateGallery(object,key,newValue) {
+  if(object.key){
+    object.key = newValue;
+  }
+   object[key] = newValue;
+   return object
+}
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+console.log(gallery);
